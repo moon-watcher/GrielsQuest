8513d86055556d5f2b67ea60df9ed99d987b85d3
moon-watcher <langares@yahoo.es>
2016-05-09 16:51:43 +0200

    SGDK dir removed

--- sgdk/sample/sprite/res/music.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MUSIC_H_
-#define _MUSIC_H_
-
-extern const u8 sonic_music[20332];
-
-#endif // _MUSIC_H_
--- sgdk/src/z80_drv1.h
+++
@@ -1,6 +0,0 @@
-#ifndef _Z80_DRV1_H_
-#define _Z80_DRV1_H_
-
-extern const u8 z80_drv1[0xB20];
-
-#endif // _Z80_DRV1_H_
