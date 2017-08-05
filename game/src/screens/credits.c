dd5d874d01ba99cc1d450e2bb5c307457b41e04d
moon-watcher <langares@yahoo.es>
2017-08-05 22:46:01 +0200

    Credits transaction

--- game/src/screens/credits.c
+++ game/src/screens/credits.c
@@ -85,11 +85,11 @@ static void writeCredits ( u8 part, u8 y_pos )
     if ( part == 2 ) writeTextCredits2();
     if ( part == 3 ) writeTextCredits3();
 
-    displayOn(1);
+    displayOn(10);
 
     waitJoySc ( 10 );
 
-    displayOff ( 1 );
+    displayOff ( 10 );
     resetScreen();
 }
 
