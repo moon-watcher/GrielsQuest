ec2527d1fdf014502749529334efaf47971790b7
moon-watcher <langares@yahoo.es>
2017-08-09 23:03:37 +0200

    Updated display.c

--- game/src/display.c
+++ game/src/display.c
@@ -1,7 +1,35 @@
 #include <genesis.h>
 
 
-static u16 cache  [ 64 ] = { [0 ... 63] = 0x0000 };
+static       u16 cache  [ 64 ] = { [0 ... 63] = 0x0000 };
+static const u16 blacks [ 64 ] = { [0 ... 63] = 0x0000 };
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+static void setDisplay ( u16 on, u16 frames, u16 *colors )
+{
+    VDP_waitVSync();
+
+    SYS_disableInts ( );
+
+	if ( frames )
+	{
+	    if ( on )
+        {
+            VDP_fadeAllTo ( (u16*) colors, frames, 0 );
+        }
+        else
+        {
+            VDP_fadeOutAll ( frames, 0 );
+        }
+	}
+
+    VDP_setPaletteColors ( 0, (u16*) colors, 64 );
+
+	SYS_enableInts ( );
+}
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -33,13 +61,13 @@ void prepareColor ( u16 index, u16 color )
 
 void displayOff ( u16 frames )
 {
-    VDP_fadeOutAll ( frames ? frames : 1, 0 );
+    setDisplay ( 0, frames, (u16*) blacks );
 }
 
 
 void displayOn ( u16 frames )
 {
-    VDP_fadeAllTo ( (u16*) cache, frames ? frames : 1, 0 );
+    setDisplay ( 1, frames, (u16*) cache );
 }
 
 
