d9647f0a1fc6d24c544b0a6c7a19f710ba5b3631
langares@yahoo.es <langares@yahoo.es>
2024-09-20 18:08:45 +0200

    Restore special chars

--- game/src/frases.c
+++ game/src/frases.c
@@ -54,51 +54,51 @@ static void _prepare_string ( u8 *str )
 
         switch ( _string[j] )
         {
-            case 250: _string[j] =  31;  break; // �
-            case 243: _string[j] =  30;  break; // �
-            case 241: _string[j] =  29;  break; // �
-            case 237: _string[j] =  28;  break; // �
-            case 233: _string[j] =  27;  break; // �
-            case 225: _string[j] =  26;  break; // �
-            case 218: _string[j] =  25;  break; // �
-            case 211: _string[j] =  24;  break; // �
-            case 209: _string[j] =  23;  break; // �
-            case 205: _string[j] =  22;  break; // �
-            case 201: _string[j] =  21;  break; // �
-            case 193: _string[j] =  20;  break; // �
-            case 191: _string[j] =  19;  break; // �
-            case 161: _string[j] =  18;  break; // �
-            case 249: _string[j] =  17;  break; // �
-            case 242: _string[j] =  16;  break; // �
-            case 231: _string[j] =  15;  break; // �
-            case 236: _string[j] =  14;  break; // �
-            case 232: _string[j] =  13;  break; // �
-            case 224: _string[j] =  12;  break; // �
-            case 217: _string[j] =  11;  break; // �
-            case 210: _string[j] =  10;  break; // �
-            case 199: _string[j] =   9;  break; // �
-            case 204: _string[j] =   8;  break; // �
-            case 200: _string[j] =   7;  break; // �
-            case 192: _string[j] =   6;  break; // �
-            case 251: _string[j] =   3;  break; // �
-            case 244: _string[j] =   2;  break; // �
-            case 238: _string[j] =   1;  break; // �
-            case 234: _string[j] = 255;  break; // �
-            case 226: _string[j] = 254;  break; // �
-            case 194: _string[j] = 253;  break; // �
-            case 212: _string[j] = 252;  break; // �
-            case 206: _string[j] = 250;  break; // �
-            case 202: _string[j] = 249;  break; // �
-            case 229: _string[j] = 248;  break; // �
-            case 196: _string[j] = 246;  break; // �
-            case 228: _string[j] = 247;  break; // �
-            case 214: _string[j] =   4;  break; // �
-            case 246: _string[j] =   5;  break; // �
-            case 197: _string[j] = 248;  break; // �
-            case 195: _string[j] = 232;  break; // �
-            case 227: _string[j] = 233;  break; // �
+            case 250: _string[j] =  31;  break; // ú
+            case 243: _string[j] =  30;  break; // ó
+            case 241: _string[j] =  29;  break; // ñ
+            case 237: _string[j] =  28;  break; // í
+            case 233: _string[j] =  27;  break; // é
+            case 225: _string[j] =  26;  break; // á
+            case 218: _string[j] =  25;  break; // Ú
+            case 211: _string[j] =  24;  break; // Ó
+            case 209: _string[j] =  23;  break; // Ñ
+            case 205: _string[j] =  22;  break; // Í
+            case 201: _string[j] =  21;  break; // É
+            case 193: _string[j] =  20;  break; // Á
+            case 191: _string[j] =  19;  break; // ¿
+            case 161: _string[j] =  18;  break; // ¡
+            case 249: _string[j] =  17;  break; // ù
+            case 242: _string[j] =  16;  break; // ò
+            case 231: _string[j] =  15;  break; // ç
+            case 236: _string[j] =  14;  break; // ì
+            case 232: _string[j] =  13;  break; // è
+            case 224: _string[j] =  12;  break; // à
+            case 217: _string[j] =  11;  break; // Ù
+            case 210: _string[j] =  10;  break; // Ò
+            case 199: _string[j] =   9;  break; // Ç
+            case 204: _string[j] =   8;  break; // Ì
+            case 200: _string[j] =   7;  break; // È
+            case 192: _string[j] =   6;  break; // À
+            case 251: _string[j] =   3;  break; // û
+            case 244: _string[j] =   2;  break; // ô
+            case 238: _string[j] =   1;  break; // î
+            case 234: _string[j] = 255;  break; // ê
+            case 226: _string[j] = 254;  break; // â
+            case 194: _string[j] = 253;  break; // Û
+            case 212: _string[j] = 252;  break; // Ô
+            case 206: _string[j] = 250;  break; // Î
+            case 202: _string[j] = 249;  break; // Ê
+            case 229: _string[j] = 248;  break; // Â
+            case 196: _string[j] = 246;  break; // Ä
+            case 228: _string[j] = 247;  break; // ä
+            case 214: _string[j] =   4;  break; // Ö
+            case 246: _string[j] =   5;  break; // ö
+            case 197: _string[j] = 248;  break; // Å
+            case 195: _string[j] = 232;  break; // Ã
+            case 227: _string[j] = 233;  break; // ã
 
-// for ���� ���� ---> portuguese
+// for ïüõã ÏÜÕÃ ---> portuguese
         }
 
         // Exception. Weird collision :?
