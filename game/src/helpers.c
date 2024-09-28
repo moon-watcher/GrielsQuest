69c36687324622c69fd40ce583b6932488d29cc1
langares@yahoo.es <langares@yahoo.es>
2024-09-28 15:11:46 +0200

    strlen_countChars()

--- game/src/helpers.c
+++ game/src/helpers.c
@@ -100,6 +100,16 @@ void VDP_setSpritePriorityGQ ( u16 index, u16 high )
 //}
 //
 
+u16 strlen_countChars(const u8 *str)
+{
+	u16 chr, counter = 0;
+
+	while (chr = (u16)*str++)
+		if (chr != 195 && chr != 194)
+			++counter;
+
+	return counter;
+}
 
 u32 my_strtol ( u8 *cadena )
 {
