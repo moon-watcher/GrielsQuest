d007e53cb3274bf57193e7ed4e6d600ee83a8a73
langares@yahoo.es <langares@yahoo.es>
2024-09-29 02:19:13 +0200

    no message

--- game/inc/helpers.h
+++ game/inc/helpers.h
@@ -12,7 +12,7 @@
 #define not     NOT
 #define isnt    ISNT
 
-#define TYPETEXT(COLOR, ABC, START) {                                 \
+#define TYPETEXT(COLOR, ABC, START) {                              \
 	u8 *s = frases_next();                                         \
 	for (u16 i = 0, l = typetext_write_init(COLOR, s); i < l; i++) \
 		switch (typetext_write_process(i, s)) {                    \
@@ -22,14 +22,6 @@
 			case BUTTON_C: goto ABC;                               \
 		}                                                          \
 }
-
-/*
-#define TYPE2(COLOR) {                                             \
-	u8 *s = frases_next();                                         \
-	for (u16 i = 0, l = typetext_write_init(COLOR, s); i < l; i++) \
-		typetext_write_process(i, s);                              \
-}
-*/
 
 //u8  *my_strtok                  ( u8 *string, u8 delimitador );
 u32  my_strtol                  ( u8 *cadena );
